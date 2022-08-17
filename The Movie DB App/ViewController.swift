bd72d98e4ddc575b720b4b087855042ed0df36c5
Adriancys Jesus Villegas Toro <adriancysvillegast@gmail.com>
2022-08-17 09:41:48 -0400

    Initial Commit

---
+++ The Movie DB App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Movie DB App
+//
+//  Created by Adriancys Jesus Villegas Toro on 17/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
