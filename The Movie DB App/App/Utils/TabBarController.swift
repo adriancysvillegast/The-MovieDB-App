95fffaa0d53e03aa826d2ed7c32605cf5bfb2cff
Adriancys Villegas <adriancysvillegast@gmail.com>
2022-08-31 21:12:23 -0400

    change button profile

--- The Movie DB App/App/Utils/TabBarController.swift
+++ The Movie DB App/App/Utils/TabBarController.swift
@@ -11,14 +11,6 @@ class TabBarController: UITabBarController {
 
     //MARK: - properties
     
-    let userRepository = UserRepository()
-    
-    private lazy var profileBotton: UIBarButtonItem = {
-        let button = UIBarButtonItem(image: UIImage(systemName: "person"), style: .done, target: self, action: #selector(goToProfile))
-        return button
-    }()
-    
-    
     //MARK: - lifecycle
     
     override func viewDidLoad() {
@@ -26,32 +18,9 @@ class TabBarController: UITabBarController {
         view.backgroundColor = .systemBackground
         UITabBar.appearance().barTintColor = .systemBackground
         tabBar.tintColor = .label
-        self.navigationItem.rightBarButtonItem = profileBotton
         navigationItem.hidesBackButton = true
         setupVCs()
 
-    }
-    
-    //MARK: - add targets
-    
-    @objc func goToProfile() {
-        
-        let alert = UIAlertController(title: "What do you want to do?", message: "", preferredStyle: .actionSheet)
-        let navigateProfile = UIAlertAction(title: "View Profile", style: .default) { _ in
-            let vc = ProfileViewController()
-            self.navigationController?.pushViewController(vc, animated: true)
-        }
-        
-        let navigateHome = UIAlertAction(title: "Log Out", style: .default) { _ in
-            self.userRepository.logOut()
-            let vc = LogInViewController()
-            self.navigationController?.pushViewController(vc, animated: true)
-        }
-        let cancel = UIAlertAction(title: "Cancel", style: .cancel)
-        alert.addAction(navigateProfile)
-        alert.addAction(navigateHome)
-        alert.addAction(cancel)
-        present(alert, animated: true)
     }
     
     //MARK: - createNavController
