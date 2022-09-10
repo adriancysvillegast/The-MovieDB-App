05d5d6e5a8d86523ea1209a0db2a4892377184af
Adriancys Villegas <adriancysvillegast@gmail.com>
2022-09-10 17:34:55 -0400

    scene edit

--- The Movie DB App/SceneDelegate.swift
+++ The Movie DB App/SceneDelegate.swift
@@ -16,23 +16,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        
-        let home = HomeViewController()
-        let navigationController = UINavigationController(rootViewController: home)
-        window?.rootViewController = navigationController
-        window?.makeKeyAndVisible()
-        
-//        if userRepository.isActiveSession(){
-//            let home = TabBarController()
-//            let navigationController = UINavigationController(rootViewController: home)
-//            window?.rootViewController = navigationController
-//            window?.makeKeyAndVisible()
-//        }else{
-//            let home = LogInViewController()
-//            let navigationController = UINavigationController(rootViewController: home)
-//            window?.rootViewController = navigationController
-//            window?.makeKeyAndVisible()
-//        }
+
+        if userRepository.isActiveSession(){
+            let home = TabBarController()
+            let navigationController = UINavigationController(rootViewController: home)
+            window?.rootViewController = navigationController
+            window?.makeKeyAndVisible()
+        }else{
+            let home = LogInViewController()
+            let navigationController = UINavigationController(rootViewController: home)
+            window?.rootViewController = navigationController
+            window?.makeKeyAndVisible()
+        }
         
     }
 
